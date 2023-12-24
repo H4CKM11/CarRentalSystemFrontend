8b7ba3b4ea0c18d95cea17e9dd15215ba153ac5c
H4CKM11 <eduardomartinez73119@gmail.com>
2023-12-24 11:35:43 -0600

    Header Created

---
+++ src/components/common/buttons/SignInButton.vue
@@ -0,0 +1,10 @@
+<template>
+    <button class="bg-white hover:bg-slate-100 border border-black text-black font-semibold py-2 px-4 rounded">
+        Sign In
+    </button>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bg-white drop-shadow-md">
+        <!-- Container for the header content -->
+        <div class="max-w-[95%] mx-auto px-4 sm:px-6 lg:px-8">
+            <!-- Flex container for the header elements -->
+            <div class="flex items-center justify-between h-16">
+                <!-- Left side of the header -->
+                <div class="flex">
+                    <img src="../../assets/images/cruise_logo.svg" class="w-24">
+                </div>
+
+                <!-- Right side of the header -->
+                <div class="flex items-center">
+                    <SignInButton />
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import SignInButton from "../common/buttons/SignInButton.vue"
+
+</script>
+
+<style scoped></style>
